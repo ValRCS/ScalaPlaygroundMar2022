0d18a1e071fdb3ec533cf41f9a0b3ffc6619b69a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-07 21:48:51 +0200

    Day 4 conditionals temperature exercise

---
+++ src/main/scala/Day4HealthExercise.scala
@@ -0,0 +1,11 @@
+object Day4HealthExercise extends App {
+  println("Temperature exercise")
+  //TODO ask person for name
+  //Ask for their temperature
+  //if temperature is below 35 print "That is a bit too cold"
+  //if temperatue is between 35 and 37 (both sides inclusive) then print "You are all right!"
+  //finally if the temperature is over 37 then print "You have a fever! Consider contacting a doctor"
+
+  //consider what would be the best way to handle this logic
+
+}
