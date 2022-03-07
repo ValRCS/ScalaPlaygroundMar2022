6483f8e61b6affb1a0946f0ad24fb08610b031e4
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-07 20:03:26 +0200

    Day 4 conditionals and exercise

---
+++ src/main/scala/Day4ExerciseBonus.scala
@@ -0,0 +1,11 @@
+object Day4ExerciseBonus extends App {
+  println("Xmas bonus calculator")
+  //TODO
+  //Ask for person's name
+  //Ask for how long they have worked at the firm
+  //Ask for monthly wage
+  //Calculate Xmas bonus if they have worked at least 2  years otherwise print sorry no bonus
+  //Bonus is years worked over 2 years * 15% of monthly wage
+  //so 5 years worked, 1000 Euros wage would give 450 Euro bonus (3 years * 150)
+}
+
