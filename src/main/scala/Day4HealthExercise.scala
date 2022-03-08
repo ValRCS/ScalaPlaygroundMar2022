389c3c097cde4b6bfad8b06b68cde64cc02cc24a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-08 19:11:22 +0200

    Health Check Exercise solution

--- src/main/scala/Day4HealthExercise.scala
+++ src/main/scala/Day4HealthExercise.scala
@@ -1,3 +1,5 @@
+import scala.io.StdIn.readLine
+
 object Day4HealthExercise extends App {
   println("Temperature exercise")
   //TODO ask person for name
@@ -7,5 +9,18 @@ object Day4HealthExercise extends App {
   //finally if the temperature is over 37 then print "You have a fever! Consider contacting a doctor"
 
   //consider what would be the best way to handle this logic
+  val name = readLine("What is your name?")
+  val temperature = readLine(s"What is your temperature $name?").toDouble
+  val lowTemp = 35
+  val highTemp = 37
+
+  if (temperature < lowTemp) {
+    println("That is a bit too cold")
+  } else if (temperature >= lowTemp && temperature <= highTemp ) {
+    println("You are all right")
+  } else {
+    println("You have a fever! Consider contacting a doctor")
+    //here you could have instructions to send an SMS to your family doctor for example
+  }
 
 }
