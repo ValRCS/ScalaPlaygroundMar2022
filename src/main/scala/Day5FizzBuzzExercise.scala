1e91997151ca68381cb342d65b4f9b171fc51663
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-08 21:24:09 +0200

    FizzBuzz Exercise

--- src/main/scala/Day5FizzBuzzExercise.scala
+++ src/main/scala/Day5FizzBuzzExercise.scala
@@ -8,4 +8,36 @@ object Day5FizzBuzzExercise extends App {
   //print number if number does not divide by 5 or 7
   //so something like this 1,2,3,4,Fizz,6,Buzz,..... 34,FizzBuzz,36,...., 99,Fizz (because 100 divides evenly by 5
   //printing can be done with number on new line like we have been doing
+  //think about making it more universal - that is allow different Fizz, Buzz, start, end values
+  val fizzNum = 5
+  val fizzText = "Fizzy"
+  val buzzNum = 7
+  val buzzText ="Buzzy"
+  val start = 20
+  val end = 75
+
+  var textBuffer = "" //for longer strings it should actually be StringBuffer type
+  for (n <- start to end) {
+    if (n % fizzNum == 0 && n % buzzNum == 0) {
+      print(fizzText+buzzText)
+      textBuffer += fizzText+buzzText
+    } else if (n % fizzNum == 0) {
+      print(fizzText)
+      textBuffer += fizzText
+
+    } else if (n % buzzNum == 0) {
+      print(buzzText)
+      textBuffer += buzzText
+    } else {
+      print(n) //when printing I do not need to convert to String, because print will do it for us
+      textBuffer += n.toString //we need to convert the number to string before string concatenation
+    }
+    if (n < end) {
+      print(",")
+      textBuffer += ","
+    }
+  }
+  println("") //so newline
+//  println("*"*40) //just some stars as divider
+  println(textBuffer)
 }
