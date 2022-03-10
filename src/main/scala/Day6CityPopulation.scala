20c328d715581d3f8e08602e7daffbf325d5457f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-10 22:01:33 +0200

    City Population Exercise

--- src/main/scala/Day6CityPopulation.scala
+++ src/main/scala/Day6CityPopulation.scala
@@ -16,9 +16,9 @@ object Day6CityPopulation extends App {
 
   //so idea is that readLine, println, all of those we would leave for other functions
   //our little conversion function would only worry about the calculations
-  println(celsiusToFahrenheit(36.6))
-  println(celsiusToFahrenheit(37))
-  println(celsiusToFahrenheit(48.5))
+//  println(celsiusToFahrenheit(36.6))
+//  println(celsiusToFahrenheit(37))
+//  println(celsiusToFahrenheit(48.5))
 
   def temperatureCalculationApp(): Unit = {
     val celsius = readLine("What is temperature in Celsius? ").toDouble
@@ -26,7 +26,7 @@ object Day6CityPopulation extends App {
     //however i could have named this celsius something else
     println(s"$celsius of Celsius is $fahrenheit of Fahrenheit.")
   }
-  temperatureCalculationApp() //no parameters required so no need for parenthesis
+//  temperatureCalculationApp() //no parameters required so no need for parenthesis
 
   //TODO 2nd main TASK - not really related to first task
   println("Function to calculate city growth")
@@ -58,10 +58,50 @@ object Day6CityPopulation extends App {
    */
   def getCityYear(p0: Int, percentage: Double, delta: Int, targetPopulation: Int):Int = {
     //add some code here....
-    9000 //FIXME //right now it returns this 9000 all the time
+    var numberOfYears = 0
+    var currentPop = p0
+    //so we are going to cut off decimal values so 1052.34 humans will be 1052
+    def nextYearPop(current: Int, percentage: Double, delta: Int):Int = (current*(1+percentage/100)+delta).toInt
+
+    def checkTargetPopulation(currentPop:Int, targetPopulation:Int= targetPopulation, p0:Int=p0):Boolean = {
+      if (p0 < targetPopulation) {
+        currentPop < targetPopulation
+      } else {
+        currentPop > targetPopulation //we go the other way
+      }
+    }
+
+    def checkCurrentPopulation(currentPop:Int, newPopulation:Int, targetPopulation:Int= targetPopulation, p0:Int=p0): Boolean = {
+      if (p0 < targetPopulation) {
+        currentPop < newPopulation
+      } else {
+        currentPop > newPopulation
+      }
+    }
+
+    var newPopulation = nextYearPop(currentPop, percentage, delta)
+
+    //lets check if we are losing people or stagnate on first year meaning nothing changes
+    if (!(checkCurrentPopulation(currentPop, newPopulation)) && checkTargetPopulation(currentPop)) {
+      numberOfYears = -1 //a bit of a workaround for our worst case scenario
+    }
+
+    //so while we are growing we are going to keep checking
+    while (checkCurrentPopulation(currentPop, newPopulation) && checkTargetPopulation(currentPop)) {
+      numberOfYears += 1
+      currentPop = newPopulation
+      newPopulation = nextYearPop(currentPop, percentage, delta)
+    }
+    //so if we stagnate
+
+    numberOfYears //FIXME //right now it returns this 9000 all the time
   }
 
   println(getCityYear(1000,2,50,1200)) // should print 3
   println(getCityYear(1000,2,-50,1200)) // should print -1
   println(getCityYear(1500000,2.5,10000,2000000)) // should print 10
+  //so searching for negative growth is currently not supported
+  println(getCityYear(1000,2,-50,800)) //this will give us -1 when we actually would reach this
+  println(getCityYear(1000,-2,-50,800)) //this will give us -1 when we actually would reach this
+
 }
