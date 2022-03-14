1f69ddf70215fe5cb295e2a8c600f9cd639e219c
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-14 21:17:06 +0200

    Full unoptimized solution of Day 7 Exercise

--- src/main/scala/Day7ExerciseSolved.scala
+++ src/main/scala/Day7ExerciseSolved.scala
@@ -6,14 +6,21 @@ object Day7ExerciseSolved extends App {
     //you will probably want to use var to store a temporary string that you keep reweriting
     //return newly created string
 
+    var trimmedString = ""
+    if (needsTrim) {
+      trimmedString = text.trim
+    } else {
+      trimmedString = text
+    }
+
     var newString = "" //so I start with an empty string and build it up
     //this approach with an empty string is completely fine on smaller strings say a few hundred characters long
     //it will get slow to add a new character to the end of a large string
     //for that we need to use a StringBuilder which is optimized for concatenation operations
     //https://www.baeldung.com/scala/stringbuilder //TODO check it out
-    for (c <- text){
+    for (c <- trimmedString){
       if (uppercaseChars.contains(c)) {
-        newString += c.toUpper
+        newString += c.toUpper //we are building up a new string one character at a time
       } else {
         newString += c
       }
@@ -23,4 +30,5 @@ object Day7ExerciseSolved extends App {
 
   println(processString("abracadabra", "cr")) //should print abRaCadabRa
   println(processString("   abracadabra  ", "cr", needsTrim = true)) //should print abRaCadabRa
+  println(processString("   Riga is a nice city isn't it?  ", "ci", needsTrim = true)) //should print abRaCadabRa
 }
