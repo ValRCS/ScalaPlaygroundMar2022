281465feae9c2f912a410725db79dbd08d798f6a
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-03-24 20:04:27 +0200

    Day 10 Character Counting exercise

---
+++ src/main/scala/Day10ExerciseCountLetters.scala
@@ -0,0 +1,9 @@
+object Day10ExerciseCountLetters extends App {
+  //TODO ask user for some sentence or word
+  //Count the number of characters in this string
+  //Store them in a mutable Map of type Char, Int
+  //print the results
+  //you can count whitespace as well
+
+  //you could store results in some sort of Sequence and then convert to Map later, but that would just complicate things
+}
