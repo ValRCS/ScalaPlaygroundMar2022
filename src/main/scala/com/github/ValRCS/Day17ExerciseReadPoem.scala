6f44006d29dbd81cddb605fff80fa459ac4175ce
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-12 17:21:04 +0300

    Day 17 Reading Files Exercise

---
+++ src/main/scala/com/github/ValRCS/Day17ExerciseReadPoem.scala
@@ -0,0 +1,12 @@
+package com.github.ValRCS
+
+object Day17ExerciseReadPoem extends App {
+
+    //TODO read stopping by poem from src/resources/stopping_by.txt
+    //TODO get poem name - it is the first line
+    //TODO get poet - it is in the 2nd line but we want just the poet
+
+    //TODO Get all lines which contain "woods" somewhere in the text
+    //so instead of startsWith we use contains
+
+}
