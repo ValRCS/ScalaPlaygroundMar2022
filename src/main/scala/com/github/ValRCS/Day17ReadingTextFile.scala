33dd80fdea8b96f15ca6f38b695a595ee1afbe1f
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-12 17:44:43 +0300

    Day 17 Reading Files Exercise with Reading

--- src/main/scala/com/github/ValRCS/Day17ReadingTextFile.scala
+++ src/main/scala/com/github/ValRCS/Day17ReadingTextFile.scala
@@ -12,8 +12,8 @@ object Day17ReadingTextFile extends App {
   //using Absolute Path first
   // Windows uses \ for separating folders - unfortunately \ is also used to escape some character \n \t
   // One solution would be to escape those \ -> \\
-  val filePath = "C:\\Users\\val-wd\\IdeaProjects\\ScalaPlaygroundMar2022\\src\\resources\\two_roads.txt"
-
+  val absoluteFilePath = "C:\\Users\\val-wd\\IdeaProjects\\ScalaPlaygroundMar2022\\src\\resources\\two_roads.txt"
+  val relativeFilePath = "src/resources/two_roads.txt"
   //print all characters one by one
 //  for (character <- Source.fromFile(filePath)) println(character) //turns out we just read a big string of characters...
 
@@ -21,17 +21,59 @@ object Day17ReadingTextFile extends App {
 //  for (line <- Source.fromFile(filePath).getLines) println(line) //we want to read lines of text not single characters
 
   //we could save the whole file into a String
-  val myPoem = Source.fromFile(filePath).mkString //so we turn a stream of characters into one big String
+//  val myPoem = Source.fromFile(absoluteFilePath).mkString //so we turn a stream of characters into one big String
+//  val myPoem = Source.fromFile(relativeFilePath).mkString //so we turn a stream of characters into one big String
+//
+//  println(myPoem)
 
-  println(myPoem)
+//  val myPoemLines = Source.fromFile(absoluteFilePath).getLines.toArray //toArray because iterator was on demand lazy
+//  val myPoemLines = Source.fromFile(relativeFilePath).getLines.toArray //toArray because iterator was on demand lazy
+//
+//  println("*"*40)
+//  println("Printing Poem line by line \n")
+//
+//  for (line <- myPoemLines) println(line)
 
-  val myPoemLines = Source.fromFile(filePath).getLines.toArray //toArray because iterator was on demand lazy
+  //TODO move me to Utilities
+  def getTextFromFile(src: String):String = {
+    val bufferedSource = Source.fromFile(src)
+    val text = bufferedSource.mkString
+    bufferedSource.close()
+    text
+  }
 
-  println("*"*40)
-  println("Printing Poem line by line \n")
+  val myText = getTextFromFile(relativeFilePath)
+  println(myText)
 
-  for (line <- myPoemLines) println(line)
+  //TODO move me to Utilities
+  def getLinesFromFile(src: String):Array[String] = {
+    val bufferedSource = Source.fromFile(src)
+    val lines = bufferedSource.getLines().toArray
+    bufferedSource.close()
+    lines
+  }
 
+  val maxLines = 5
+  println(s"FIRST $maxLines lines")
+  val myLines = getLinesFromFile(relativeFilePath)
+  myLines.slice(0,maxLines).foreach(println)
+  //same result as previous operation but here we create the substring first, above we printed line by line
+  println(myLines.slice(0,maxLines).mkString("\n")) //this should be slightly faster
+
+  println("Lines starting with And")
+  val andLines = myLines.filter(_.startsWith("And"))
+  andLines.foreach(println)
+
+  val roadsLines = myLines.filter(_.contains("roads")) //text can be anywhere in the line
+  roadsLines.foreach(println)
+
+  //alternative to filter is to use yield it is more flexible because you can also perform some mappings
+  //with filter we would have to use map as well
+  val endsWithComma = for (line <- myLines if line.endsWith(",")) yield line.toUpperCase
+  endsWithComma.foreach(println)
+
+  val firstLine = myLines(0) //could also use myLines.head
+  println(firstLine)
 
 
 
