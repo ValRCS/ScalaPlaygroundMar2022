8de49969f06a8bc122e34c7fc460c69a149741d1
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-16 15:10:29 +0300

    Day 19 Exercise on reading multiple URLs and saving files

---
+++ src/main/scala/com/github/ValRCS/Day19TestingRegex.scala
@@ -0,0 +1,5 @@
+package com.github.ValRCS
+
+object Day19TestingRegex extends App {
+
+}
--- src/main/scala/com/github/ValRCS/Flower.scala
+++ src/main/scala/com/github/ValRCS/Flower.scala
@@ -2,7 +2,12 @@ package com.github.ValRCS
 
 class Flower(flowerType:String, val color:String) {
   println(s"Created a new flower of type: $flowerType with color $color")
+  val flowerLength:Int  = flowerType.length
   //more methods here
   //more construction work here
+  def save(dst:String=""):Unit = {
+    //TODO actually save the flower!
+  }
+
 }
 
