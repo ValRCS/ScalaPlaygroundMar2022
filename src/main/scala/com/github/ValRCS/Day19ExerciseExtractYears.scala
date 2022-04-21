578a8d1e24c890e35dbab507eab35eb7c72ed13b
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-21 19:28:06 +0300

    Day 19 RegEx Exercise

---
+++ src/main/scala/com/github/ValRCS/Day19ExerciseExtractYears.scala
@@ -0,0 +1,9 @@
+package com.github.ValRCS
+
+object Day19ExerciseExtractYears extends App {
+  //TODO extract all years into a Array(or list or vector) of Integers
+  //From the following src
+  val src="src/resources/ChristieA_Poirot_Investigates.txt"
+
+  //you can test your regex here first: https://regex101.com/ or somewhere else
+}
