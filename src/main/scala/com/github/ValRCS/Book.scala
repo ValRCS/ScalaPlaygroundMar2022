aa6d3f0b2ea1d52ced22c6dfd0ff60fd505249c9
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-04-28 16:25:17 +0300

    Day 22 Reading XML into Case Class

---
+++ src/main/scala/com/github/ValRCS/Book.scala
@@ -0,0 +1,4 @@
+package com.github.ValRCS
+
+case class BookUnit(id:String, author:String, title:String, genre:String, price:Double)
+//TODO finish the case class with the right fields from books.xml book elemen
