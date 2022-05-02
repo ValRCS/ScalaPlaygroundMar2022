d6caea27e9c516548c674bb5a10b28319fa175fd
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-05-02 21:20:08 +0300

    Day 23 SQL exercise solved

--- src/resources/db/SQL_Exercises_Day23.sql
+++ src/resources/db/SQL_Exercises_Day23.sql
@@ -1,3 +1,26 @@
+
+-- SELECT ALL non USA customers
+SELECT * FROM customers c
+WHERE Country != 'USA' ;
+
+-- SELECT ONLY German customers
+SELECT * FROM customers c 
+WHERE Country = 'Germany' ; 
+
+-- SELECT employees (different table) who are sales agents
+
+SELECT * FROM employees e 
+WHERE Title LIKE "%SALES%AGENT%";
+
+-- SELECT unique billing countries from invoices table
+
+SELECT DISTINCT BillingCountry 
+FROM invoices i
+ORDER BY BillingCountry ASC; 
+
+SELECT * FROM customers;
+
+
 SELECT * FROM tracks t 
 ORDER BY Name DESC
 LIMIT 25
