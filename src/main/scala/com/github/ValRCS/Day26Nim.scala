7466bc84d150365795df133e8282e81d31b98cc7
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-05-08 16:26:34 +0300

    Day 26 NIM exercise

--- src/main/scala/com/github/ValRCS/Day26Nim.scala
+++ src/main/scala/com/github/ValRCS/Day26Nim.scala
@@ -2,14 +2,27 @@ package com.github.ValRCS
 
 import scala.io.StdIn.readLine
 
+//TODO exercise
+//Create class Nim (or NimGame or NimState) - if you can come up with a better name please do
+//you could use case class if you want
+//Objects created from this class will hold ALL information particular to a single game of NIM
+//
+//create two at least two methods
+//TODO one will be removeMatches which will perform a move of units allowed by the rules of the game
+
+//TODO 2nd method would be to print game status let's call this method showStatus - how many matches are in the pile, whose turn it is,
+//
+//TODO bonus: objects created from this class should also have a ArrayBuffer of moves
+//so each time removeMatches is called this buffer is updated, thus we have an exact log of game moves
+
+
 object Day26Nim extends App {
  //TODO implement basic version of https://en.wikipedia.org/wiki/Nim
   //https://en.wikipedia.org/wiki/Nim#The_21_game
 
   //TODO setup/config - set data/state what is needed for the application
   //TODO main application/game loop - it could be a loopless - if you process data only once
-  //TODO cleanup - close database connetions, files - sometime no cleanup is needed
-
+  //TODO cleanup - close database connections, files etc
   //No plan survives first contact with the enemy - who said it first?
   //It is normal (especially Agile development) to adjust as you development
 
@@ -32,6 +45,8 @@ object Day26Nim extends App {
   var currentState = startingCount
   var isPlayerATurn = true //so A goes first
 
+  //TODO create a new object holding all the information necessary for a game nim from this class Nim
+  //val nimGame = new Nim(TODO....)
 
   def clampMove(move: Int, min:Int, max:Int, verbose: Boolean  = true): Int = {
     if (move > max) {
@@ -52,11 +67,13 @@ object Day26Nim extends App {
     //show the game state
     println(s"Currently there are $currentState matches on the table")
     val move = readLine(s"How many matches do you want to take $currentPlayer? (1-3) ").toInt //TODO error checking
-    //TODO limit the number of matches according to the rules
+
     val safeMove = clampMove(move, minMove, maxMove)
-    currentState -= safeMove
+    currentState -= safeMove //TODO replace this with removeMatches method call
+
     isPlayerATurn = !isPlayerATurn //toggle trick to change a boolean to reverse version of present
     //play the game
+    //TODO print game status by using showStatus method
   }
   //TODO PvC - player versus computer you will need to add some logic to the computer
 
@@ -66,6 +83,7 @@ object Day26Nim extends App {
   val winner = if (isPlayerATurn) playerA else playerB
   val loser = if (!isPlayerATurn) playerA else playerB
   println(s"Game ended. Congratulations $winner! Better luck next time $loser.")
+  //print game status again
   //TODO implement multiple games
 
 
