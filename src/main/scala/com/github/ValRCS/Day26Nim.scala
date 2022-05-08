90a0e2e168a6f3c1f9cf233a70fd342387339f2e
Valdis Saulespurens <valdis.s.coding@gmail.com>
2022-05-08 12:30:56 +0300

    Day 26 NIM working first version of the game

--- src/main/scala/com/github/ValRCS/Day26Nim.scala
+++ src/main/scala/com/github/ValRCS/Day26Nim.scala
@@ -21,17 +21,40 @@ object Day26Nim extends App {
   val minMove = 1
   val maxMove = 3
 
-  //TODO get player names
   val playerA = readLine("Player A what is your name?")
   val playerB = readLine("Player B what is your name?")
 
   println(s"Player A -  $playerA and Player B - $playerB let us play NIM!")
 
+  //inevitably in most applications we will have some state that we want to keep track of
+  //here it is simple enough state that we can use a few variables
+  //at some point we will want to structure this game/app state into a separate object based on some class
+  var currentState = startingCount
+  var isPlayerATurn = true //so A goes first
+
 
   //main loop - while there are some matches play on
   //TODO implement PvP - player versus player - computer only checks the rules
+  while (currentState > gameEndCondition) {
+    val currentPlayer = if (isPlayerATurn) playerA else playerB
+    //show the game state
+    println(s"Currently there are $currentState matches on the table")
+    val move = readLine(s"How many matches do you want to take $currentPlayer? (1-3) ").toInt //TODO error checking
+    //TODO limit the number of matches according to the rules
+    currentState -= move
+    isPlayerATurn = !isPlayerATurn //toggle trick to change a boolean to reverse version of present
+    //play the game
+  }
   //TODO PvC - player versus computer you will need to add some logic to the computer
 
+  //end cleanup here we just print some game state and congratulations
+  //TODO add saving to Database, stats etc
 
-  //no clean up at the moment
+  val winner = if (isPlayerATurn) playerA else playerB
+  val loser = if (!isPlayerATurn) playerA else playerB
+  println(s"Game ended. Congratulations $winner! Better luck next time $loser.")
+  //TODO implement multiple games
+
+
+
 }
